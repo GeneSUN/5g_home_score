ebf0dcd7b9c9dcc4c5cb2f41ca67bd8db16305ed
Zhe Sun <injure21@gmail.com>
2024-04-01 09:35:46 -0400

    Update featureToScore.py

--- featureToScore.py
+++ featureToScore.py
@@ -7,6 +7,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 import numpy as np
 import sys 
+sys.path.append('/usr/apps/vmas/scripts/ZS') 
+from MailSender import MailSender
 
 def convert_string_numerical(df, String_typeCols_List): 
 
@@ -15,15 +17,15 @@ def convert_string_numerical(df, String_typeCols_List):
     return df 
 
 @udf(returnType=FloatType())
-def calculate_DataScore(switch_count_sum, ServicetimePercentage, fiveg_usage_percentage): 
+def calculate_DataScore(switch_count_sum, fiveg_usage_percentage, sqrt_data_usage): 
 
     weight_switch_count_sum = 0.2 
     
-    weight_ServicetimePercentage = 0.5 
+    weight_fiveg_usage_percentage = 0.5 
     
-    weight_fiveg_usage_percentage = 0.3 
+    weight_sqrt_data_usage = 0.3 
 
-    if switch_count_sum is None and ServicetimePercentage is None and fiveg_usage_percentage is None: 
+    if switch_count_sum is None and fiveg_usage_percentage is None and sqrt_data_usage is None: 
         return None 
     else: 
         total_weight = 0 
@@ -31,12 +33,12 @@ def calculate_DataScore(switch_count_sum, ServicetimePercentage, fiveg_usage_per
         if switch_count_sum is not None: 
             score += weight_switch_count_sum * switch_count_sum 
             total_weight += weight_switch_count_sum 
-        if ServicetimePercentage is not None: 
-            score += weight_ServicetimePercentage * ServicetimePercentage 
-            total_weight += weight_ServicetimePercentage 
         if fiveg_usage_percentage is not None: 
             score += weight_fiveg_usage_percentage * fiveg_usage_percentage 
             total_weight += weight_fiveg_usage_percentage 
+        if sqrt_data_usage is not None: 
+            score += weight_sqrt_data_usage * sqrt_data_usage 
+            total_weight += weight_sqrt_data_usage 
         return score / total_weight 
         
 @udf(returnType=FloatType())
@@ -86,8 +88,8 @@ def featureToScore(homeScore_df, feature, reverse=False):
     windowSpec = Window.partitionBy("cpe_model_name") 
 
     medianCol = percentile_approx(feature, 0.5, 10000).over(windowSpec) 
-    lower5Col = percentile_approx(feature, 0.05, 100).over(windowSpec) 
-    top95Col = percentile_approx(feature, 0.95, 100).over(windowSpec) 
+    lower5Col = percentile_approx(feature, 0.01, 10000).over(windowSpec) 
+    top95Col = percentile_approx(feature, 0.99, 10000).over(windowSpec) 
      
     df_with_percentiles = homeScore_df.withColumn(f"{feature}_median", medianCol)\
                                     .withColumn(f"{feature}_lower_5_percentile", lower5Col)\
@@ -126,19 +128,21 @@ def featureToScore(homeScore_df, feature, reverse=False):
 
     return df_capped 
 
-def featureToScoreShfit(homeScore_df, feature): 
+def featureToScoreShfit(homeScore_df, feature, reverse=False): 
 
     windowSpec = Window.partitionBy("cpe_model_name") 
 
     # Calculate the median of the feature for each cpe_model_name 
 
     medianCol = F.expr(f"percentile_approx({feature}, 0.5, 10000)").over(windowSpec) 
-    diff_to_target = 60 - medianCol 
-
-    # Create a new column by adding or subtracting the difference from the original feature 
-    df_with_scaled_feature = homeScore_df.withColumn(f"{feature}_median", medianCol)\
-                                        .withColumn(f"scaled_{feature}", F.round(F.col(feature) + diff_to_target,2) ) 
-
+    if not reverse: 
+        diff_to_target = 60 - medianCol 
+        df_with_scaled_feature = homeScore_df.withColumn(f"{feature}_median", medianCol)\
+                                            .withColumn(f"scaled_{feature}", F.round(F.col(feature) + diff_to_target,2) ) 
+    else:
+        diff_to_target = 60 + medianCol 
+        df_with_scaled_feature = homeScore_df.withColumn(f"{feature}_median", medianCol)\
+                                            .withColumn(f"scaled_{feature}", F.round( -F.col(feature) + diff_to_target,2) ) 
     # Cap values between 0 and 100 
 
     df_capped = df_with_scaled_feature.withColumn(  
@@ -148,13 +152,10 @@ def featureToScoreShfit(homeScore_df, feature):
         .otherwise(F.col(f"scaled_{feature}")) 
 
     ) 
-
- 
-
     return df_capped 
 
 if __name__ == "__main__":
-    
+    mail_sender = MailSender()
     spark = SparkSession.builder\
             .appName('5gHome_featureToScore')\
             .config("spark.sql.adapative.enabled","true")\
@@ -164,42 +165,57 @@ if __name__ == "__main__":
     
     day_before = 1
     d = ( date.today() - timedelta(day_before) ).strftime("%Y-%m-%d")
+    try:
+        homeScore_df = spark.read.parquet(hdfs_pd + "/user/ZheS/5g_homeScore/join_df/"+d)
+        homeScore_df = convert_string_numerical(homeScore_df, ["fourg_total_mb","fiveg_total_mb", "fiveg_usage_percentage"])
+        cpe_models_to_keep =  ["ARC-XCI55AX", "ASK-NCQ1338FA", "WNC-CR200A", "ASK-NCQ1338", "FSNO21VA", "NCQ1338E",'Others'] 
+        homeScore_df = homeScore_df.withColumn("sqrt_data_usage", F.sqrt( col("fourg_total_mb")+col("fiveg_total_mb") ) )\
+                                    .withColumn("ServicetimePercentage", col("ServicetimePercentage")*100 )\
+                                    .withColumn("cpe_model_name", 
+                                            when(col("cpe_model_name").isin(cpe_models_to_keep), col("cpe_model_name")) 
+                                                .otherwise("Others")) 
+        
+        features = ['imei', 'imsi', 'mdn_5g', 'cust_id', 'cpe_model_name', 'fiveg_usage_percentage', 'downloadresult', 'uploadresult', 'latency', 'sn', 'ServicetimePercentage', 'switch_count_sum', 'avg_CQI', 'avg_MemoryPercentFree', 'log_avg_BRSRP', 'log_avg_SNR', 'log_avg_5GSNR', 'LTERACHFailurePercentage', 'LTEHandOverFailurePercentage', 'NRSCGChangeFailurePercentage']
+        key_features = ['imei', 'imsi', 'mdn_5g', 'cust_id','sn', 'cpe_model_name']
+        scaled_features = ["downloadresult","uploadresult","avg_CQI","log_avg_BRSRP","log_avg_SNR","log_avg_5GSNR","sqrt_data_usage"]
+        scaled_features_reverse = ["latency"]
+        shift_features_reverse = ["fiveg_usage_percentage"]
+        raw_features_reverse = ["ServicetimePercentage","switch_count_sum"]
 
-    homeScore_df = spark.read.parquet(hdfs_pd + "/user/ZheS/5g_homeScore/join_df/"+d)
-    homeScore_df = convert_string_numerical(homeScore_df, ["fourg_total_mb","fiveg_total_mb", "fiveg_usage_percentage"])
-    cpe_models_to_keep =  ["ARC-XCI55AX", "ASK-NCQ1338FA", "WNC-CR200A", "ASK-NCQ1338", "FSNO21VA", "NCQ1338E",'Others'] 
-    homeScore_df = homeScore_df.withColumn("ServicetimePercentage", col("ServicetimePercentage")*100 )\
-                                .withColumn("cpe_model_name", 
-                                        when(col("cpe_model_name").isin(cpe_models_to_keep), col("cpe_model_name")) 
-                                            .otherwise("Others")) 
-    
-    features = ['imei', 'imsi', 'mdn_5g', 'cust_id', 'cpe_model_name', 'fiveg_usage_percentage', 'downloadresult', 'uploadresult', 'latency', 'sn', 'ServicetimePercentage', 'switch_count_sum', 'avg_CQI', 'avg_MemoryPercentFree', 'log_avg_BRSRP', 'log_avg_SNR', 'log_avg_5GSNR', 'LTERACHFailurePercentage', 'LTEHandOverFailurePercentage', 'NRSCGChangeFailurePercentage']
-    key_features = ['imei', 'imsi', 'mdn_5g', 'cust_id','sn', 'cpe_model_name']
-    scaled_features = ["downloadresult","uploadresult","avg_CQI","log_avg_BRSRP","log_avg_SNR","log_avg_5GSNR"]
-    scaled_features_reverse = ["latency"]
-    raw_features = ["fiveg_usage_percentage"]
-    raw_features_reverse = ["ServicetimePercentage","switch_count_sum"]
+        df_score = homeScore_df 
 
-    df_score = homeScore_df 
+        for feature in scaled_features: 
+            df_score = featureToScore(df_score, feature) 
 
-    for feature in scaled_features: 
-        df_score = featureToScore(df_score, feature) 
+        for feature in scaled_features_reverse: 
+            df_score = featureToScore(df_score, feature, reverse=True) 
 
-    for feature in scaled_features_reverse: 
-        df_score = featureToScore(df_score, feature, reverse=True) 
+        #for feature in raw_features:
+        #    df_score = df_score.withColumn( f"scaled_{feature}", F.round( col(feature),2 ) )
 
-    for feature in raw_features:
-        df_score = df_score.withColumn( f"scaled_{feature}", F.round( col(feature),2 ) )
+        for feature in raw_features_reverse:
+            #df_score = df_score.withColumn( f"scaled_{feature}", F.round( 100 - col(feature) ) )
+            df_score = df_score.withColumn(f"scaled_{feature}",  
+                                    F.round(when(100 - col(feature) < 0, 0) 
+                                            .otherwise(100 - col(feature)), 2))\
+                                .withColumn(  
+                                            f"scaled_{feature}",  
+                                            F.when(F.col(f"scaled_{feature}") < 0, 0) 
+                                            .when(F.col(f"scaled_{feature}") > 100, 100) 
+                                            .otherwise(F.col(f"scaled_{feature}")) 
+                                        )
+        for feature in shift_features_reverse: 
+            df_score = featureToScoreShfit(df_score, feature, reverse=True) 
+        
 
-    for feature in raw_features_reverse:
-        #df_score = df_score.withColumn( f"scaled_{feature}", F.round( 100 - col(feature) ) )
-        df_score = df_score.withColumn(f"scaled_{feature}",  
-                                F.round(when(100 - col(feature) < 0, 0) 
-                                        .otherwise(100 - col(feature)), 2)) 
-    
-    df_score = df_score.withColumn("dataScore", F.round( calculate_DataScore("scaled_switch_count_sum", "scaled_ServicetimePercentage", "scaled_fiveg_usage_percentage"),2)  )
-    df_score = df_score.withColumn("NetworkScore", F.round( calculate_NetworkScore("scaled_log_avg_BRSRP", "scaled_avg_CQI", "scaled_log_avg_SNR", "scaled_log_avg_5GSNR", "scaled_uploadresult", "scaled_downloadresult", "scaled_latency"),2) )
+        df_score = df_score.withColumn("dataScore", F.round( calculate_DataScore("scaled_switch_count_sum", "scaled_fiveg_usage_percentage", "scaled_sqrt_data_usage"),2)  )
+        df_score = df_score.withColumn("NetworkScore", F.round( calculate_NetworkScore("scaled_log_avg_BRSRP", "scaled_avg_CQI", "scaled_log_avg_SNR", "scaled_log_avg_5GSNR", "scaled_uploadresult", "scaled_downloadresult", "scaled_latency"),2) )
 
-    df_score.repartition(10)\
-            .write.mode("overwrite")\
-            .parquet( hdfs_pd + "/user/ZheS/5g_homeScore/final_score/" + d )
+        df_score.repartition(10)\
+                .write.mode("overwrite")\
+                .parquet( hdfs_pd + "/user/ZheS/5g_homeScore/final_score/" + d )
+    except Exception as e:
+        print(e)
+        mail_sender.send( send_from ="featureToScore@verizon.com", 
+                            subject = f"featureToScore failed !!! at {d}", 
+                            text = e)
